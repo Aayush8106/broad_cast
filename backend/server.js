19648bde9ad6bdeac08548c7459f149d397b03cf
Aayush8106 <aayushp8106@gmail.com>
2026-07-01 12:51:53 +0530

    chek

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,12 @@ import bcrypt from "bcrypt";
 
 dotenv.config();
 
+
+console.log("SMTP_HOST:", process.env.SMTP_HOST);
+console.log("SMTP_PORT:", process.env.SMTP_PORT);
+console.log("SMTP_USER:", process.env.SMTP_USER);
+console.log("SMTP_PASS:", process.env.SMTP_PASS ? "Loaded" : "Missing");
+
 const app=express();
 app.set("trust proxy", 1);
 const PORT=process.env.PORT;
